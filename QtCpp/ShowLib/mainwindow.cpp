924088904f570d1eb3ab67f88f2989fb359c9756
JZFamily <hao931126@126.com>
2020-03-23 21:12:14 +0800

    0323

--- QtCpp/ShowLib/mainwindow.cpp
+++ QtCpp/ShowLib/mainwindow.cpp
@@ -1,6 +1,34 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "qdebug.h"
+#include <QtSql>
+#include <QFileDialog>
+#include <stack>
+
+const size_t code_file_suffix_size = 7;
+QString code_file_suffix[] = {"cpp","h","cxx","hpp",
+                              "js",
+                              "py",
+                              "c"
+                             };
+const size_t img_file_suffix_size = 5;
+QString img_file_suffix[] = {"png","jpg","icon","svg","ico"};
+
+bool isFileType(QString suffix,QString file_suffix_array[],const size_t count)
+{
+    bool ret = false;
+    qDebug() << sizeof (file_suffix_array);
+    for(size_t i = 0; i < count;i++)
+    {
+        if(suffix == file_suffix_array[i])
+        {
+            ret = true;
+            break;
+        }
+    }
+    return ret;
+}
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -13,7 +41,88 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::actionOpen()
+
+void MainWindow::on_actionOpen_triggered()
 {
-    qDebug() << "actionOpen ";
+    const QString dirName(QFileDialog::getExistingDirectory(this, "Choose a directory", m_curDir));
+    if (!dirName.isEmpty()) {
+        QDir d(dirName);
+        //setRepository(d.absolutePath());
+        m_curDir = d.absolutePath();
+        qDebug() << d.absolutePath();
+        getfiles(m_curDir);
+    }
+}
+
+void MainWindow::on_actionClose_triggered()
+{
+
+}
+
+QStringList MainWindow::getfiles(const QString &dir_path)
+{
+    ui->treeWidget->clear();
+    ui->treeWidget->update();
+    QStringList get_files;
+    QDir dir(dir_path);
+    if(!dir.exists())
+    {
+        qDebug() << "it is not true dir_path";
+    }
+    QIcon icon_file = m_iconMap[FILE_FOLDER_TYPE];//(":res/resource/files.png");
+    std::stack<std::pair<QString,QTreeWidgetItem *>> stack;
+    QTreeWidgetItem *cur = new QTreeWidgetItem(ui->treeWidget);
+    cur->setIcon(0,icon_file);
+    cur->setText(0,dir.dirName());
+    ui->treeWidget->setHeaderHidden(true);
+    ui->treeWidget->setAnimated(true);
+    ui->treeWidget->addTopLevelItem(cur);
+    stack.push(std::make_pair(dir.absolutePath(),cur));
+    while(!stack.empty())
+    {
+        auto pair = stack.top();
+        stack.pop();
+        QDir curDir(pair.first);
+        auto pCurItem = pair.second;
+        auto infoList = curDir.entryInfoList(QDir::Files | QDir::Dirs | QDir::NoDotAndDotDot);
+        qDebug()<< "root " <<pair.first;
+        for(int i = 0; i < infoList.size(); i++)
+        {
+            QFileInfo subdir(infoList.at(i));
+            if(subdir.absoluteFilePath() == pair.first)
+            {
+                qDebug()<< "continue" <<subdir.absoluteFilePath();
+                continue;
+            }
+            qDebug()<<subdir.absoluteFilePath();
+            QTreeWidgetItem *subItem = new QTreeWidgetItem(pCurItem);
+            if(subdir.isDir())
+            {
+                subItem->setIcon(0,icon_file);
+                qDebug()<< "push" <<subdir.absoluteFilePath();
+                stack.push(std::make_pair(subdir.absoluteFilePath(),subItem));
+            }
+            else
+            {
+                auto suffix = subdir.suffix();
+                if(isFileType(suffix,code_file_suffix,code_file_suffix_size))
+                {
+                    subItem->setIcon(0,m_iconMap[FILE_CODE_TYPE]);
+                }
+                else if(isFileType(suffix,img_file_suffix,img_file_suffix_size))
+                {
+                    subItem->setIcon(0,m_iconMap[FILE_IMG_TYPE]);
+                }
+                else
+                {
+                    subItem->setIcon(0,m_iconMap[FILE_BLANK_TYPE]);
+                }
+            }
+            subItem->setText(0,subdir.fileName());
+            pCurItem->insertChild(i,subItem);
+
+        }
+    }
+    ui->treeWidget->update();
+    return get_files;
 }
--- QtCpp/ShowLib/mainwindow.h
+++ QtCpp/ShowLib/mainwindow.h
@@ -2,10 +2,26 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <map>
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
+
+enum FILE_TYPE
+{
+EMPTY_TYPE = 0,
+FILE_AI_TYPE ,
+FILE_BLANK_TYPE,
+FILE_CODE_TYPE ,
+FILE_EXE_TYPE ,
+FILE_IMG_TYPE,
+FILE_MUSIC_TYPE ,
+FILE_PDF_TYPE,
+FILE_TXT_TYPE,
+FILE_VIDEO_TYPE,
+FILE_ZIP_TYPE,
+FILE_FOLDER_TYPE,
+};
 
 class MainWindow : public QMainWindow
 {
@@ -14,7 +30,16 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+private slots:
+    void on_actionOpen_triggered();
+
+    void on_actionClose_triggered();
+private:
+    void readSqlite();
+    QStringList getfiles(const QString &dir_path);
 private:
     Ui::MainWindow *ui;
+    QString             m_curDir;
+    std::map<FILE_TYPE,QIcon>  m_iconMap;
 };
 #endif // MAINWINDOW_H
